2622fabaf1200855c2c204461c84b1a707131b7c
Joel <jnfeinstein@gmail.com>
2014-11-08 17:41:46 -0800

    DB: Streamline migrations

--- scripts/migrate.go
+++ scripts/migrate.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
 	_ "parsley/app/models"
 	. "parsley/db"
 )
 
 func main() {
 	fmt.Println("Beginning migration")
-	numMigrated, err := Migrate()
+
+	db, err := NewDatabaseConnection()
+	if err != nil {
+		fmt.Println("Error migrating database")
+		os.Exit(1)
+	}
+
+	db.AutoMigrate(AllModels...)
+
 	if err != nil {
 		fmt.Println("Error migrating database")
 	} else {
-		fmt.Printf("Migrated %d models\n", numMigrated)
+		fmt.Printf("Migrated %d models\n", len(AllModels))
 	}
 }
