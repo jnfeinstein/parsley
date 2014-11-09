81802b9c0af6bd4a242b089b3f1b754ee16a0c82
Joel <jnfeinstein@gmail.com>
2014-11-08 22:41:20 -0800

    IndexController: Add the index route
    It will send a logged in user to the ParsleyController.

--- app/controllers/index_controller.go
+++ app/controllers/index_controller.go
@@ -3,14 +3,19 @@ package controllers
 import (
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/oauth2"
+	"net/http"
 	"parsley/internals"
 )
 
 type IndexController struct{}
 
 func (i *IndexController) Initialize(m *martini.ClassicMartini) {
-	m.Get("/", func(tokens oauth2.Tokens) {
-
+	m.Get("/", func(w http.ResponseWriter, r *http.Request, r render.Render, tokens oauth2.Tokens) {
+		if tokens.IsExpired() {
+			r.HTML(200, "index", nil)
+		} else {
+			http.Redirect(w, r, "/parsley", http.StatusTemporaryRedirect)
+		}
 	})
 }
 
