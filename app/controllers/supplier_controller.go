58f641b6520994bb894b208a05e0d44836c044cd
Asa Zernik <asa.zernik@gmail.com>
2014-11-08 21:33:06 -0800

    supplier_controller: Add methods to create suppliers

--- app/controllers/supplier_controller.go
+++ app/controllers/supplier_controller.go
@@ -1,9 +1,11 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
 	"log"
+	"net/http"
 	"parsley/app/models"
 	"parsley/db"
 	"parsley/internals"
@@ -23,6 +25,33 @@ func (s SuppliersController) Initialize(m *martini.ClassicMartini) {
 		r.JSON(200, suppliers)
 	})
 
+	m.Post("/suppliers", func(r render.Render, req *http.Request) {
+		name := req.PostFormValue("name")
+		if name == "" {
+			r.Error(400)
+			return
+		}
+		supplier := models.Supplier{Name: name}
+		conn.Create(&supplier)
+		r.JSON(200, supplier)
+	})
+	m.Put("/suppliers/(?P<id>\\d+)", func(r render.Render, params martini.Params, req *http.Request) {
+		var id int64
+		if _, err := fmt.Sscan(params["id"], id); err != nil {
+			r.Error(400)
+			return
+		}
+		name := req.PostFormValue("name")
+		if name == "" {
+			r.Error(400)
+			return
+		}
+		supplier := models.Supplier{}
+		conn.First(&supplier, id)
+		supplier.Name = name
+		conn.Save(supplier)
+		r.JSON(200, supplier)
+	})
 }
 
 func init() {
