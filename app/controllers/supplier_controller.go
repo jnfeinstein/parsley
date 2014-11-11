3865de341474b46f9be8c0a64e9e66fbc605a8f5
Asa Zernik <asa.zernik@gmail.com>
2014-11-11 00:30:37 -0800

    controllers/supplier: Create per-user views of this table
    Whether a resource is global or in a per-user sandbox is not obvious. In
    the future, we might want some fields (e.g. the supplier's
    name/existence) to be public, and others (like contact info) stay
    private, but if we want to sandbox a whole DB table this is how to do
    it.
    
    Possible future tweaks:
    
    * We could have some globally-visible rows (ingredient info from the
    FDA database, for example) in a generally sandboxed table by having a
    special global UserId (e.g. 0).
    
    * Another thing we might want to do is sandbox relations (e.g. even for
    the shared ingredients, each user has different suppliers).

--- app/controllers/supplier_controller.go
+++ app/controllers/supplier_controller.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
+	"github.com/martini-contrib/sessions"
 	"net/http"
+	"parsley/app/handlers"
 	"parsley/app/models"
 	"parsley/db"
 	"parsley/internals"
@@ -14,13 +16,23 @@ type SuppliersController struct{}
 
 func (sc SuppliersController) Initialize(m *martini.ClassicMartini) {
 
-	m.Get("/suppliers/list", func(r render.Render, conn *db.Connection) {
+	m.Get("/suppliers/list", handlers.UserRequired, func(r render.Render, conn db.Connection, s sessions.Session) {
+		user_id, ok := s.Get("user_id").(int64)
+		if !ok {
+			r.Error(500)
+			return
+		}
 		suppliers := []models.Supplier{}
-		conn.Find(&suppliers)
+		conn.Where("user_id = ?", user_id).Find(&suppliers)
 		r.JSON(200, suppliers)
 	})
 
-	m.Post("/suppliers", func(r render.Render, req *http.Request, conn *db.Connection) {
+	m.Post("/suppliers", func(r render.Render, req *http.Request, conn db.Connection, s sessions.Session) {
+		user_id, ok := s.Get("user_id").(int64)
+		if !ok {
+			r.Error(500)
+			return
+		}
 		supplier := models.Supplier{}
 
 		if json.NewDecoder(req.Body).Decode(&supplier) != nil {
@@ -30,6 +42,7 @@ func (sc SuppliersController) Initialize(m *martini.ClassicMartini) {
 			r.Error(400)
 			return
 		}
+        supplier.UserId = user_id
 		conn.Save(supplier) // updates if ID provided
 		r.JSON(200, supplier)
 	})
