f3dac843b0770297212b60e04e774ed530b801c5
Joel <jnfeinstein@gmail.com>
2014-11-10 22:21:40 -0800

    Add goose for migrations
    Heroku wasn't happy with the migration strategy that I had developed.
    This is more traditional, using a separate app for the migrations.
    
    Note: It requires a custom version of gorm, which adds the gorm.Wrap
    command.  I have requested it be pulled into the main repo but in
    the meantime I will maintain it under github.com/jnfeinstein/gorm.

---
+++ db/migrations/20141110221610_AddUsersAndEmails.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+CREATE TABLE users
+(
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP
+);
+
+CREATE TABLE emails
+(
+  id BIGSERIAL PRIMARY KEY,
+  user_id BIGINT NOT NULL,
+  address VARCHAR(255) NOT NULL UNIQUE,
+  created_at TIMESTAMP
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS emails;
