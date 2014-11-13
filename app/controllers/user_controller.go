0b5f02e817c6ecc745268cb26659faa4f9aebf8f
Joel <jnfeinstein@gmail.com>
2014-11-12 23:01:57 -0800

    Move OAuth2 config back into user_controller.go
    Url() can change after flags are parsed, and the staticly defined
    version we had previously was using a stale Env() value for Url().
    This fixes the issue by generating the Url() when it is needed,
    which will be sometime after initialization.

--- app/controllers/user_controller.go
+++ app/controllers/user_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/go-martini/martini"
+	gooauth2 "github.com/golang/oauth2"
 	"github.com/martini-contrib/oauth2"
 	"parsley/config"
 	"parsley/internals"
@@ -10,7 +11,12 @@ import (
 type UserController struct{}
 
 func (u *UserController) Initialize(m *martini.ClassicMartini) {
-	m.Use(oauth2.Google(&config.GoogleOAuthConfig))
+	m.Use(oauth2.Google(&gooauth2.Options{
+		ClientID:     "242182491314-3kv6lg5gbqi3tcfp42fr2s1kvqkg09ih.apps.googleusercontent.com",
+		ClientSecret: "QJUY1sHMJhoZlJ6E10W9cJqz",
+		RedirectURL:  config.Url() + "/oauth2callback",
+		Scopes:       []string{"profile email"},
+	}))
 }
 
 func init() {
